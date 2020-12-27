848ce3641ade716f192423125f0ae1bf74bc7e4f
Ralf Sigmund <ralf@moia.io>
2020-12-27 22:04:40 +0100

    convert brightness url parameter to string

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const host = '0.0.0.0';
 const port = 3000;
 let brightness = 0;
 
-const requestListener = function (req, res) {
+const requestListener = function (req, resA) {
    
     let inc = parseInt(new URL(req.url, `http://${req.headers.host}`).searchParams.get("inc"));
     console.log(inc);
@@ -18,21 +18,21 @@ const requestListener = function (req, res) {
             brightness = parseInt(parsed_data['brightness']); 
             console.log('current brightness:' + brightness);  
             brightness = brightness + inc;
-            let request = http.get('http://192.168.178.41/light/0?brightness=' + brightness, (res) => {
+            let request = http.get('http://192.168.178.41/light/0?brightness=' + brightness.toString, (res) => {
                 if (res.statusCode !== 200) {
                     console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
                     res.resume();
+                    resA.writeHead(200);
+                    resA.end("Failed To set brightness to " + brightness);
                     return;
                 } else {
                     console.log(res);
+                    resA.writeHead(200);
+                    resA.end("Set brightness to " + brightness);
                 }});
         });
     });
-
-
-
-    res.writeHead(200);
-    res.end("Set brightness to " + brightness);
+    
 };
 
 const server = http.createServer(requestListener);
